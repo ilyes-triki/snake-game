c53e95cd20f35431303540da7aa901b05ee55c11
ilyes-triki <70591709+ilyes-triki@users.noreply.github.com>
2022-08-22 23:48:35 +0100

    first commit

---
+++ snake/js/food.js
@@ -0,0 +1,42 @@
+import { onSnake , expandSnake} from './snake.js'
+import {randomGridPosition} from './grid.js'
+
+let food = getRandomFoodPosition()
+let difficulty = document.getElementById("difficulty")
+let diffUp = document.querySelector(".fa-caret-up-diff")
+let diffDown = document.querySelector(".fa-caret-down-diff")
+diffUp.addEventListener('click' , e=> {
+    difficulty.innerHTML =  parseInt(difficulty.innerHTML) + 1
+  })
+  diffDown.addEventListener('click' , e=> {
+      if(parseInt(diff.innerHTML) >= 2 ){
+        difficulty.innerHTML =  parseInt(difficulty.innerHTML) - 1
+      }
+    })
+   let EXPANSION_RATE = difficulty.innerHTML
+export function getDifficulty ( ) {
+ EXPANSION_RATE = difficulty.innerHTML
+}
+export function update() {
+    if(onSnake(food)){
+        expandSnake(EXPANSION_RATE)
+        food = getRandomFoodPosition()
+    }
+   
+}
+export function draw(gameBoard) {
+   
+     const foodElement = document.createElement('div')
+     foodElement.style.gridColumnStart = food.x
+     foodElement.style.gridRowStart = food.y
+     foodElement.classList.add('food') 
+     gameBoard.appendChild(foodElement)   
+    }
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+      newFoodPosition = randomGridPosition() 
+    }
+    return newFoodPosition
+    
+}
---
+++ snake/js/snake.js
@@ -0,0 +1,78 @@
+import { getInputDirection } from "./input.js"
+
+let speed = document.getElementById('speed')
+let speedUp = document.querySelector(".fa-caret-up-speed")
+let speedDown = document.querySelector(".fa-caret-down-speed")
+const snakeBody = [
+    {x:11 , y:11},  
+]
+let newSegments = 0 
+ let score = document.getElementById('score')
+
+speedUp.addEventListener('click' , e=> {
+    speed.innerHTML =  parseInt(speed.innerHTML) + 1
+  })
+  speedDown.addEventListener('click' , e=> {
+      if(parseInt(speed.innerHTML) >= 2 ){
+          speed.innerHTML =  parseInt(speed.innerHTML) - 1
+      }
+    })
+
+
+
+
+export let SNAKE_SPEED = speed.innerHTML
+
+
+export function gameSpeed () {
+     SNAKE_SPEED = speed.innerHTML
+}
+
+export function update() {
+    addSegments()
+    const inputDirection = getInputDirection()
+   for (let i = snakeBody.length - 2 ; i >= 0 ; i--) {
+    snakeBody[i+1] = {... snakeBody[i]} 
+    }
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
+}
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => { 
+     const snakeElement = document.createElement('div')
+     snakeElement.style.gridColumnStart = segment.x
+     snakeElement.style.gridRowStart = segment.y
+     snakeElement.classList.add('snake') 
+     gameBoard.appendChild(snakeElement)   
+    })
+    }
+
+export function expandSnake(amount) {
+newSegments += amount
+}   
+export function onSnake(position , {ignoreHead = false }= {}) {
+    return snakeBody.some((segment, index)=> {
+        if (ignoreHead && index === 0 ) return false
+        return equalPositions(segment , position)
+    })
+} 
+function equalPositions(pos1 , pos2) {
+    return pos1.x === pos2.x && pos2.y === pos1.y
+}
+function addSegments() {
+    for(let i = 0 ; i < newSegments ; i++) {
+snakeBody.push({...snakeBody[snakeBody.length - 1] })
+    }
+    newSegments = 0
+    
+}
+export function getSnakeHead () {
+    return snakeBody[0]
+}
+export function snakeIntersection () {
+    return onSnake(snakeBody[0] ,{ ignoreHead :true})
+
+}
+export function gameScore () {
+    return score.innerHTML = snakeBody.length - 1
+}
